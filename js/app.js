d9758b70b17297bc7ced02850dc2ad721f04201a
DuLinart <eduardo@linarte.mx>
2016-11-21 00:43:32 -0600

    Se agregaron las secciones de footer y los estilos requeridos

--- js/app.js
+++ js/app.js
@@ -1,3 +1,13 @@
 $(document).foundation() 
 
+const $offCanvas = $('#offCanvas')
+const $sticky = $('#sticky')
+$offCanvas.find('li').click(function (ev) {
+	$offCanvas.foundation('close')
+	setTimeout(function (){
+		$sticky.css('left', '0px')
+	}, 300)
+	
+})
+
 
